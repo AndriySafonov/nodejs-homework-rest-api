1c62e97dd1747ff3d5e7a4aef6a21da56e24e6e0
AndriySafonov <andreygold18@gmail.com>
2023-07-03 10:37:12 +0300

    removed some comments

--- models/contactModel.js
+++ models/contactModel.js
@@ -20,13 +20,8 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
-    // owner: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "user",
-    //   required: true,
-    // },
-  },
-  { versionKey: false, timestamps: true }
+  }
+  // { versionKey: false, timestamps: true }
 );
 
 contactSchema.post("save", handleMongooseError);
