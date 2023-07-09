da8e9b83fb80e0f8001f7581cc708b587cff5ca5
AndriySafonov <andreygold18@gmail.com>
2023-07-09 18:55:11 +0300

    created getCurrent

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { registerSchema, loginSchema } = require("../../schemas");
 const ctrl = require("../../controllers/users");
 
@@ -10,5 +10,7 @@ router.post("/register", validateBody(registerSchema), ctrl.register);
 
 // signin
 router.post("/login", validateBody(loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
 
 module.exports = router;
